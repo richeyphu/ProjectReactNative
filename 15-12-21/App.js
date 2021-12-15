900c8456553a41fa2c93f0af0ef9677063d920c4
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2021-12-15 12:00:03 +0700

    15/12/21

---
+++ 15-12-21/App.js
@@ -0,0 +1,15 @@
+// 15/12/2564
+
+import React from 'react';
+
+// import AlertExample from './components/AlertExample';
+import ModalExample from './components/ModalExample';
+
+const App = () => {
+  return (
+    // <AlertExample/>
+    <ModalExample />
+  );
+};
+
+export default App;
