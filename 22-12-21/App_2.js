a380b65718e50a1fa4b238e2c6a0e28fca93382a
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2021-12-22 12:03:16 +0700

    WebView and Stack Navigation

---
+++ 22-12-21/App_2.js
@@ -0,0 +1,15 @@
+// 22/12/2564
+
+import React from 'react';
+
+// import WebViewExample from './components/WebViewExample';
+import WebViewLoading from './components/WebViewLoading';
+
+const App = () => {
+  return (
+    // <WebViewExample />
+    <WebViewLoading />
+  );
+};
+
+export default App;
