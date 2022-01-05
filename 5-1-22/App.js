5e2c0df29c4af3cfa417812c7993b492c13f3343
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-01-05 10:24:35 +0700

    Add files via upload

---
+++ 5-1-22/App.js
@@ -0,0 +1,68 @@
+// 5/1/2565
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import FirstPage from './screens/FirstPage';
+import SecondPage from './screens/SecondPage';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#FF69B4',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="First"
+          component={FirstPage}
+          options={{title: 'หน้าหลัก'}}
+        />
+        <Stack.Screen
+          name="Second"
+          component={SecondPage}
+          options={{title: 'เกี่ยวกับเรา'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  inputStyle: {
+    width: '80%',
+    height: 44,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#DBDBD6',
+  },
+});
