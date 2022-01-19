8c5f725cf6921a4e1190a81aaa3cb92b7aee9239
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-01-19 10:57:01 +0700

    projectReact Navigation

---
+++ 19-1-22/App.js
@@ -0,0 +1,45 @@
+// 19/01/2565
+
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#0096DA',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'หน้าหลัก'}}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{title: 'เกี่ยวกับเรา'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ 19-1-22/screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      {/* <Ionicons name="home" size={30} color="skyblue" /> */}
+      <Text>หน้าหลัก</Text>
+      <Button
+        title="Go to About"
+        onPress={() =>
+          navigation.navigate('About', {
+            email: 'de.phurit_st@tni.ac.th',
+          })
+        }
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
