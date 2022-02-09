8a5950d99cf9c0cc1185242ed7e28c6430e12dc2
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-02-09 11:56:09 +0700

    9/2/22 Part 1

--- 9-2-22/Part 1/screens/MenuScreen.js
+++ 9-2-22/Part 1/screens/MenuScreen.js
@@ -15,7 +15,7 @@ import {
   Switch,
 } from 'native-base';
 
-const MenuScreen = () => {
+const MenuScreen = ({navigation}) => {
   return (
     <ScrollView style={{flex: 1}}>
       <View>
@@ -30,27 +30,38 @@ const MenuScreen = () => {
         </Text>
         {/* Native base code starts here */}
         <Content>
-          <ListItem icon>
+          {/* Home Page */}
+          <ListItem
+            icon
+            style={{marginBottom: 10, marginTop: 10}}
+            onPress={() => {
+              navigation.navigate('HomeStack');
+            }}>
             <Left>
               <Button style={{backgroundColor: '#FF9501'}}>
                 <Icon active name="home" />
               </Button>
             </Left>
             <Body>
-              <Text>หน้าหลัก</Text>
+              <Text>Home Page</Text>
             </Body>
             <Right>
               <Icon active name="arrow-forward" />
             </Right>
           </ListItem>
-          <ListItem icon>
+          {/* Product Page */}
+          <ListItem
+            icon
+            onPress={() => {
+              navigation.navigate('ProductStack');
+            }}>
             <Left>
               <Button style={{backgroundColor: '#007AFF'}}>
                 <Icon active name="cube" />
               </Button>
             </Left>
             <Body>
-              <Text>สินค้า</Text>
+              <Text>Product</Text>
             </Body>
             <Right>
               <Icon active name="arrow-forward" />
